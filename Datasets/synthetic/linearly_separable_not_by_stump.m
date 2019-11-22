d5ba5d658b13f97a37eb5f16322c693d77382da4
jsneal <jsneal@bu.edu>
2019-11-21 19:31:16 -0500

    Previously the index for the -1 class was incorrect

--- Datasets/synthetic/linearly_separable_not_by_stump.m
+++ Datasets/synthetic/linearly_separable_not_by_stump.m
@@ -6,18 +6,18 @@ data = zeros(100, 3);
 
 for i = 1:100
     data(i, 1) = randi(100);
-    data(i, 2) = randi([data(i, 1), data(i, 1) + randi([10,100])]);
+    data(i, 2) = randi([data(i, 1) + 1, data(i, 1) + randi([10,100])]);
     data(i, 3) = 1;
 end
 
 for j = 1:100
     data(100+j, 2) = randi(100);
-    data(100+j, 1) = randi([data(i, 1), data(i, 1) + randi([10,100])]);
+    data(100+j, 1) = randi([data(100+j, 2) + 1, data(100+j, 2) + randi([10,100])]);
     data(100+j, 3) = -1;
 end
 
-% figure(1)
-% gscatter(data(:, 1), data(:, 2), data(:, 3), 'rb')
+figure(1)
+gscatter(data(:, 1), data(:, 2), data(:, 3), 'rb')
 
 save("linearly_separable_not_by_stump.mat", "data");
 
