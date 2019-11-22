2b3387d55eaa8d3b236c63c288fda1d60e72a184
jsneal <jsneal@bu.edu>
2019-11-22 17:09:21 -0500

    Updated documentation in calculate_gs and the test script for calculate_gs

--- Functions/test_calculate_gs.m
+++ Functions/test_calculate_gs.m
@@ -1,9 +1,9 @@
 % test_calculate_gs
 
-load('C:\Users\jsnea\Desktop\EC503\Project\ec503_boosting_project\Datasets\synthetic\data_linear.mat');
-
-data = dataset';
-all_gs = calculate_gs(data);
+% load('C:\Users\jsnea\Desktop\EC503\Project\ec503_boosting_project\Datasets\synthetic\data_linear.mat');
+% 
+% data = dataset';
+% all_gs = calculate_gs(data);
 % 
 % thresh1_x2 = linspace(min(data(:, 2)), max(data(:, 2)), 100)';
 % thresh2_x1 = linspace(min(data(:, 1)), max(data(:, 1)), 100)';
@@ -11,10 +11,21 @@ all_gs = calculate_gs(data);
 % thresh1 = all_gs(1, 1).*ones(100, 1);
 % thresh2 = all_gs(2, 1).*ones(100, 1);
 
-figure(1)
-gscatter(data(:, 1), data(:, 2), data(:, 3), 'rb')
+% figure(1)
+% gscatter(data(:, 1), data(:, 2), data(:, 3), 'rb')
 
-load('C:\Users\jsnea\Desktop\EC503\Project\ec503_boosting_project\Datasets\synthetic\linearly_separable_not_by_stump.mat');
+% load('C:\Users\jsnea\Desktop\EC503\Project\ec503_boosting_project\Datasets\synthetic\linearly_separable_not_by_stump.mat');
+% 
+% all_gs = calculate_gs(data);
+% 
+% figure(1)
+% gscatter(data(:, 1), data(:, 2), data(:, 3), 'rb')
+
+% load('C:\Users\jsnea\Desktop\EC503\Project\ec503_boosting_project\Datasets\synthetic\circular_dataset.mat');
+% data = data_circular;
+% all_gs = calculate_gs(data);
+% 
+% figure(1)
+% gscatter(data(:, 1), data(:, 2), data(:, 3), 'rb')
 
 
-all_gs = calculate_gs(data);
