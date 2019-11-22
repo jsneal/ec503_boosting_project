04cd0e0e842716818dda10d2a99bbf3dec7d263f
Nadim El Helou <45805207+nadimelhelou@users.noreply.github.com>
2019-11-21 20:04:05 -0500

    Add files via upload

---
+++ Functions/ourAdaBoost.m
@@ -0,0 +1,22 @@
+%% AdaBoost John, Nadim, Karim
+load('data_linear.mat'); data_linear = dataset'; clear dataset;
+%figure; gscatter(data_linear(:,1),data_linear(:,2),data_linear(:,3),'rb','+o');
+load('circular_dataset.mat');
+%figure; gscatter(data_circular(:,1),data_circular(:,2),data_circular(:,3),'rb','+o');
+load('linearly_separable_not_by_stump.mat'); data_tilted = data; clear data;
+%figure; gscatter(data_tilted(:,1),data_tilted(:,2),data_tilted(:,3),'rb','+o');
+
+%% AdaBoost on Circular Dataset
+n = size(data_circular,1);
+T = 10;
+weights = 1/n*ones(n,1);
+all_gs = calculate_gs(data_circular);
+alphas = zeros(T,1);
+classifiers = zeros(T,3);
+
+for t=1:T
+    [best_feature, best_treshold, best_smaller_is, min_error] = calculate_best_g(data_circular, weights, all_gs);
+    alphas(t,1) = 0.5*log((1-min_error)/min_error);
+    classifiers(t,:) = [best_feature, best_treshold, best_smaller_is];
+    weights = update_weights(data_circular, weights, best_feature, best_treshold, best_smaller_is, min_error);
+end
