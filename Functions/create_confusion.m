dedfe459e81a342bbf5067609a94ee9012216265
karimkbu <48109433+karimkbu@users.noreply.github.com>
2019-12-07 15:31:18 -0500

    Add files via upload

---
+++ Functions/create_confusion.m
@@ -0,0 +1,21 @@
+function create_confusion (test_set, alphas, classifiers)
+
+% Creates a confusion matrix given a test dataset, alphas and classifiers
+
+rows = size(test_set,1);
+columns = size(test_set,2);
+known = test_set(columns,:);
+predicted = zeros(rows,1);
+
+for i=1:rows
+    predicted(i,1) = decision (test_set(i,1:columns-1), alphas, classifiers);
+end
+
+confusionchart(known,predicted);
+
+end
+
+
+
+
+
